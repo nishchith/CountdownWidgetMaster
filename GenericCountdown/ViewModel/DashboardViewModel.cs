3edd91b9ba241d8b7c8ffbb75fc7780513383867
Nishchith <iarneel@gmail.com>
2014-09-09 02:16:39 +0530

    views VMs

---
+++ GenericCountdown/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace GenericCountdown.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class DashboardViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the DashboardViewModel class.
+        /// </summary>
+        public DashboardViewModel()
+        {
+        }
+    }
+}
--- GenericCountdown/ViewModel/ViewModelLocator.cs
+++ GenericCountdown/ViewModel/ViewModelLocator.cs
@@ -29,29 +29,46 @@ namespace GenericCountdown.ViewModel
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
-            if (ViewModelBase.IsInDesignModeStatic)
-            {
-                SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
-            }
-            else
-            {
-                SimpleIoc.Default.Register<IDataService, DataService>();
-            }
-
+            // Register those ViewModels which should be setup asa the app launch
+            // this also delays the launch
             SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<DashboardViewModel>();
+            SimpleIoc.Default.Register<SettingViewModel>();
+            SimpleIoc.Default.Register<CountdownListViewModel>();
         }
 
         /// <summary>
         /// Gets the Main property.
         /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
-            "CA1822:MarkMembersAsStatic",
-            Justification = "This non-static member is needed for data binding purposes.")]
+         //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+         //   "CA1822:MarkMembersAsStatic",
+         //   Justification = "This non-static member is needed for data binding purposes.")]
         public MainViewModel Main
         {
             get
             {
                 return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+        public DashboardViewModel Dashboard
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<DashboardViewModel>();
+            }
+        }
+        public SettingViewModel Setting
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<SettingViewModel>();
+            }
+        }
+        public CountdownListViewModel CountdownList
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<CountdownListViewModel>();
             }
         }
 
