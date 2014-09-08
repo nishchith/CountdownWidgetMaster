3278a7953c2033060ed85fcf859982690a8b67ca
Nishchith <iarneel@gmail.com>
2014-09-08 23:54:03 +0530

    init

---
+++ GenericCountdown/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GenericCountdown.Resources;
+
+namespace GenericCountdown
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
